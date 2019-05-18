7fcaefcc0dd783344b05add8cfe21cdee3cfe429
SYtang98 <36919402+SYtang98@users.noreply.github.com>
2019-05-18 14:22:47 +0800

    change fomatting

--- IOT_wifi_work_half_way.ino
+++ IOT_wifi_work_half_way.ino
@@ -58,46 +58,31 @@ break;
 
 }
 
-}
 void sendCommand(String command, int maxTime, char readReplay[]) {
 
-Serial.print(countTrueCommand);
-Serial.print(". at command => ");
-Serial.print(command);
-Serial.print(" ");
-while(countTimeCommand < (maxTime*1))
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while (countTimeCommand < (maxTime * 1)) {
+    esp8266.println(command);               //at+cipsend
+    if (esp8266.find(readReplay)) {         //OK
+      found = true;
+      break;
+    }
+    countTimeCommand++;
+  }
 
-{
-
-esp8266.println(command);//at+cipsend
-if(esp8266.find(readReplay))//ok
-{
-
-found = true;
-break;
-
-}
-countTimeCommand++;
-
-}
-if(found == true)
-{
-
-Serial.println("OYI");
-
-countTrueCommand++;
-countTimeCommand = 0;
-
-}
-if(found == false)
-{
-
-Serial.println("Fail");
-countTrueCommand = 0;
-countTimeCommand = 0;
-
-}
-found = false;
-
-}
+  if (found == true) {
+    Serial.println("OK");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
   
+  if (found == false){
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  found = false;
+}
